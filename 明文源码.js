ceb957803de8e57c9d38da237dcbe020c9d0fb9c
kingrouter <154909398+kingrouter@users.noreply.github.com>
2025-02-10 21:41:08 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'b806cdf9-b36b-4f7b-8128-5bf2bdbcb152';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
